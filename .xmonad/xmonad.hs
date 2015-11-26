71721ae1bbd1a51bba88422a39f0fdc92d0a196c
Jason Schulz <jason@schulz.name>
2015-11-25 23:16:52 -0800

    update xmonad config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,7 +6,7 @@ import XMonad.Prompt.AppLauncher(launchApp)
 import XMonad.Prompt.Shell(prompt)
 import XMonad.Prompt.Shell(shellPrompt)
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig(additionalKeys, additionalKeysP)
 import System.IO
 
 main = do
@@ -23,25 +23,31 @@ main = do
       , ppUrgent= xmobarColor "#dc322f" ""
       , ppVisible = wrap "(" ")"
       }
-    , terminal = "/usr/bin/uxterm /usr/bin/tmux"
+    , terminal = "/usr/bin/urxvtc -e /usr/bin/tmux"
     } `additionalKeys`
-    [    ((mod1Mask .|. shiftMask, xK_BackSpace), spawn "/usr/bin/uxterm")
+    [    ((mod1Mask .|. shiftMask, xK_BackSpace), spawn "/usr/bin/urxvtc")
        , ((mod1Mask .|. shiftMask, xK_z), spawn "/usr/bin/xscreensaver-command -lock")
        , ((mod1Mask .|. shiftMask, xK_p), shellPrompt defaultXPConfig
-           { bgColor = "#1b1b1b"
+           { bgColor = "#373737"
            , fgColor = "#657b83"
            , borderColor = "#222222"
            }
          )
-       , ((mod1Mask .|. shiftMask, xK_m), prompt "/usr/bin/uxterm -e mux" defaultXPConfig
-           { bgColor = "#1b1b1b"
+       , ((mod1Mask .|. shiftMask, xK_m), prompt "/usr/bin/urxvtc -e mux" defaultXPConfig
+           { bgColor = "#373737"
            , fgColor = "#657b83"
            , borderColor = "#222222"
            })
        , ((mod1Mask .|. shiftMask, xK_r), prompt "/bin/sh rdesktop" defaultXPConfig
-           { bgColor = "#1b1b1b"
+           { bgColor = "#373737"
            , fgColor = "#657b83"
            , borderColor = "#222222"
            })
     ] 
+     `additionalKeysP`
+    [    ("<XF86TouchpadToggle>", spawn "~/bin/synaptics/syntoggle")
+       , ("<XF86AudioMute>", spawn "/usr/bin/pactl set-sink-mute alsa_output.pci-0000_00_1b.0.analog-stereo toggle")
+       , ("<XF86AudioRaiseVolume>", spawn "/usr/bin/pactl set-sink-volume alsa_output.pci-0000_00_1b.0.analog-stereo +1%")
+       , ("<XF86AudioLowerVolume>", spawn "/usr/bin/pactl set-sink-volume alsa_output.pci-0000_00_1b.0.analog-stereo -1%")
+    ]
 
